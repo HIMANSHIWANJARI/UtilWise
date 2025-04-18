f70e47c1d6695ba31753fa33b751b6981e155433
Prathisthapandey12 <2022csb1105@iitrpr.ac.in>
2025-04-18 17:36:17 +0530

    made cganges in add_expenses.dart

--- lib/screens/add_screens/add_expense.dart
+++ lib/screens/add_screens/add_expense.dart
@@ -312,7 +312,7 @@ class ExpenseData extends State<ExpenseScreen> {
   void initState() {
     super.initState();
     loadMembers();
-    dateController.text = "";
+    dateController.text = DateFormat('yyyy-MM-dd').format(DateTime.now());
 
   }
 
@@ -343,6 +343,10 @@ class ExpenseData extends State<ExpenseScreen> {
 
     if (widget.isFromObjectPage) {
       objectDropDown = widget.objectName;
+      selectedSubCategory =
+          categoryData[objectDropDown]!.first;
+      creatorMember = myEmail;
+
     } else if (providerCommunity
         .communityObjectMap[communityDropDown]!.isNotEmpty) {
       objectDropDown = providerCommunity.communityObjectMap[communityDropDown]![
@@ -415,6 +419,7 @@ class ExpenseData extends State<ExpenseScreen> {
                         });
                       },
                     ),
+
                   SizedBox(
                     height: 10,
                   ),
@@ -439,7 +444,7 @@ class ExpenseData extends State<ExpenseScreen> {
                       onChanged: (String? newValue) {
                         setState(() {
                           objectDropDown = newValue!;
-                          selectedSubCategory = null; // Reset selected sub-category
+                          selectedSubCategory = categoryData[objectDropDown]!.first;
                         });
                         providerCommunity.objectListen(
                             communityDropDown, objectDropDown);
@@ -478,38 +483,9 @@ class ExpenseData extends State<ExpenseScreen> {
     });
   },
 ),
-
-SizedBox(
-  height: 10,
-),
-DropdownButtonFormField<String>(
-    value: creatorMember,
-    isExpanded: true,
-    decoration: const InputDecoration(
-      icon: Icon(Icons.person),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-      ),
-      hintText: ("Paid By"),
-    ),
-    items: (availableMembers.isEmpty )
-        ? [DropdownMenuItem<String>(value: '', child: Text('No members available'))]
-        : availableMembers.map((email) {
-      return DropdownMenuItem<String>(
-        value: email,
-        child: Text(email == myEmail ? 'me ($email)' : email),
-      );
-    }).toList(),
-    onChanged: (String? value) {
-      setState(() {
-
-        creatorMember = value;
-        paidBy = value!;
-      });
-    },
-  ),
-
-            SizedBox(height : 10),
+                  SizedBox(
+                    height: 10,
+                  ),
                   TextFormField(
                     decoration: const InputDecoration(
                       icon: Icon(Icons.currency_rupee_outlined),
@@ -524,6 +500,39 @@ DropdownButtonFormField<String>(
                   SizedBox(
                     height: 10,
                   ),
+DropdownButtonFormField<String>(
+  value: creatorMember,
+  isExpanded: true,
+  decoration: const InputDecoration(
+    icon: Icon(Icons.person),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    ),
+    hintText: "Paid By",
+  ),
+  items: (availableMembers.isEmpty)
+      ? [
+          const DropdownMenuItem<String>(
+            value: '',
+            child: Text('Loading members ...'),
+          )
+        ]
+      : availableMembers.map((email) {
+          return DropdownMenuItem<String>(
+            value: email,
+            child: Text(email == myEmail ? 'me ($email)' : email),
+          );
+        }).toList(),
+  onChanged: availableMembers.isEmpty
+      ? null
+      : (String? value) {
+          setState(() {
+            creatorMember = value;
+            paidBy = value!;
+          });
+        },
+),
+            SizedBox(height : 10),
     DropdownButtonFormField<String>(
     decoration: InputDecoration(
       icon: Icon(Icons.people_outline),
